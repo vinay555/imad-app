4df5e124d81a5862e8f444f7fffff762df6a2d13
vinay555 <kandula.vinay555@gmail.com>
2017-08-09 19:49:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function (req,req));
-res.send('article-one is requested and is serverd here')
+app.get('/article-one', function (req, res){
+res.send('article-one is requested and is serverd here');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
